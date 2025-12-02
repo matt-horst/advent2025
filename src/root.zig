7b82f79ad9bfe55ae949f6ea07f4ef63b808bb64
Matt Horst <mattahorst@gmail.com>
2025-12-02 10:55:01 -0700

    solves day 2 part 1

--- src/root.zig
+++ src/root.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub const InputError = error{InvalidInput};
 
-pub fn day1Part1(input: []const u8) InputError!i32 {
+pub fn day1Part1(allocator: std.mem.Allocator, input: []const u8) InputError![]const u8 {
     const range = 100;
     var position: i32 = 50;
     var count: i32 = 0;
@@ -33,10 +33,11 @@ pub fn day1Part1(input: []const u8) InputError!i32 {
         }
     }
 
-    return count;
+    const ans = std.fmt.allocPrint(allocator, "{d}", .{count}) catch return InputError.InvalidInput;
+    return ans;
 }
 
-pub fn day1Part2(input: []const u8) InputError!i32 {
+pub fn day1Part2(allocator: std.mem.Allocator, input: []const u8) InputError![]const u8 {
     const range = 100;
     var position: i32 = 50;
     var count: i32 = 0;
@@ -79,5 +80,28 @@ pub fn day1Part2(input: []const u8) InputError!i32 {
         }
     }
 
-    return count;
+    const ans = std.fmt.allocPrint(allocator, "{d}", .{count}) catch return InputError.InvalidInput;
+    return ans;
+}
+
+pub fn day2Part1(allocator: std.mem.Allocator, input: []const u8) InputError![]const u8 {
+    var count: u64 = 0;
+
+    var it = std.mem.splitScalar(u8, input[0 .. input.len - 1], ',');
+    while (it.next()) |pair| {
+        const idx = std.mem.indexOfScalar(u8, pair, '-') orelse return InputError.InvalidInput;
+        const lhs = std.fmt.parseInt(usize, pair[0..idx], 10) catch return InputError.InvalidInput;
+        const rhs = std.fmt.parseInt(usize, pair[idx + 1 ..], 10) catch return InputError.InvalidInput;
+
+        var buf: []u8 = std.heap.page_allocator.alloc(u8, pair.len - idx) catch return InputError.InvalidInput;
+        for (lhs..rhs + 1) |i| {
+            const curr = std.fmt.bufPrint(buf[0..], "{d}", .{i}) catch return InputError.InvalidInput;
+            if (std.mem.eql(u8, curr[0 .. curr.len / 2], curr[curr.len / 2 ..])) {
+                count += @intCast(i);
+            }
+        }
+    }
+
+    const ans = std.fmt.allocPrint(allocator, "{d}", .{count}) catch return InputError.InvalidInput;
+    return ans;
 }
