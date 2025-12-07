0a01e363717ff58ea8d2a318f40657861f850d9e
Matt Horst <mattahorst@gmail.com>
2025-12-06 23:32:01 -0700

    solves day 7 part 2

--- src/day7.zig
+++ src/day7.zig
@@ -44,11 +44,10 @@ pub fn part1(allocator: std.mem.Allocator, input: []const u8) AdventError![]cons
 
                 if (is_split) total += 1;
             }
-
         }
-            const tmp = prev;
-            prev = curr;
-            curr = tmp;
+        const tmp = prev;
+        prev = curr;
+        curr = tmp;
         print(prev);
     }
 
@@ -61,6 +60,75 @@ const Loc = enum(u8) { empty = '.', beam = '|', splitter = '^' };
 fn print(line: []const Loc) void {
     for (line) |c| {
         std.debug.print("{c}", .{@intFromEnum(c)});
+    }
+    std.debug.print("\n", .{});
+}
+
+pub fn part2(allocator: std.mem.Allocator, input: []const u8) AdventError![]const u8 {
+    var it = std.mem.splitScalar(u8, input[0..input.len - 1], '\n');
+    const first_line = it.first();
+    const width = first_line.len;
+    const start = std.mem.indexOfScalar(u8, first_line, 'S') orelse return AdventError.ParseError;
+
+    var prev: []i64 = allocator.alloc(i64, width) catch return AdventError.OutOfMemory;
+    defer allocator.free(prev);
+
+    for (0..width) |i| {
+        prev[i] = if (i == start) 1 else 0;
+    }
+
+    var curr: []i64 = allocator.alloc(i64, width) catch return AdventError.OutOfMemory;
+    defer allocator.free(curr);
+
+    print_part2(prev);
+    while (it.next()) |line| {
+        for (0.., line) |i, c| {
+            curr[i] = switch (c) {
+                '.' => 0,
+                '^' => -1,
+                else => return AdventError.ParseError,
+            };
+        }
+
+        for (0.., prev) |i, p| {
+            if (p <= 0) continue;
+
+            if (curr[i] >= 0) {
+                curr[i] += p;
+            } else if (curr[i] < 0) {
+                if (i > 0 and curr[i - 1] >= 0) {
+                    curr[i - 1] += p;
+                }
+
+                if (i < width and curr[i + 1] >= 0) {
+                    curr[i + 1] += p;
+                }
+            }
+        }
+        print_part2(curr);
+        const tmp = prev;
+        prev = curr;
+        curr = tmp;
+    }
+
+    var total: i64 = 0;
+    for (prev) |p| {
+        if (p > 0) total += p;
+    }
+
+    const buf = std.fmt.allocPrint(allocator, "{d}", .{total}) catch return AdventError.OutOfMemory;
+    return buf;
+}
+
+fn print_part2(line: []const i64) void {
+    for (line) |c| {
+        if (c == 0) {
+            std.debug.print(".", .{});
+        } else if (c == -1) {
+            std.debug.print("^", .{});
+        } else {
+            std.debug.print("{d}", .{c});
+        }
     }
     std.debug.print("\n", .{});
 }
